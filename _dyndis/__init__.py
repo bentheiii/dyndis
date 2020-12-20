d81af5b5053916edc0ae8b82f8aaca0e0e003fa3
ben avrahami <dontcallme@illcall.you>
2020-12-20 20:27:34 +0200

    .

--- _dyndis/__init__.py
+++
@@ -1,7 +0,0 @@
-from dyndis.multidispatch import MultiDispatch, RawNotImplemented
-from dyndis.type_keys.type_key import Self
-from dyndis.type_keys.delegates import UnboundAttr, UnboundMap
-from dyndis.type_keys.unbound_friend import UnboundFriend
-from dyndis._version import __version__
-
-__all__ = ['MultiDispatch', 'RawNotImplemented', 'Self', 'UnboundAttr', 'UnboundMap', 'UnboundFriend', '__version__']
